88d93d4a015987d89f10b727b5f750c7d1b77580
Dhaniya1 <ndohare11@gmail.com>
2026-07-12 00:55:57 +0530

    parsed rss news articles

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from backend!")
+from urllib.parse import quote
+
+import feedparser
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/search")
+def search(query: str):
+
+    rss_url = f"https://news.google.com/rss/search?q={quote(query)}"
+
+    feed = feedparser.parse(rss_url)
+
+    articles = []
+
+    for article in feed.entries:
+        articles.append(
+            {
+                "title": article.title,
+                "url": article.link,
+                "published": article.published,
+            }
+        )
+
+    return articles
